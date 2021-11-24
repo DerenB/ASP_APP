8e398dcfc01a17d2b88058ddfe67fb70e13d27af
Deren B <dbozerprogramming@gmail.com>
2021-11-24 17:58:18 -0500

    Creating the Connection String

--- API/Startup.cs
+++ API/Startup.cs
@@ -16,8 +16,9 @@ using Microsoft.OpenApi.Models;
 
 namespace API {
     public class Startup {
-        public Startup(IConfiguration configuration) {
-            Configuration = configuration;
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config) {
+            _config = config;
         }
 
         public IConfiguration Configuration { get; }
@@ -25,7 +26,7 @@ namespace API {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddDbContext<DataContext>(options => {
-                options.UseSqlite("Connection string");
+                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
             services.AddSwaggerGen(c => {
